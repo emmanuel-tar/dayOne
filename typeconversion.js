9248e3b01924b7b743d0124468e9b6062dc4d43e
Emmanuel <emmanueltar14@gmail.com>
2023-03-21 08:47:33 +0100

    type conversion

---
+++ typeconversion.js
@@ -0,0 +1,16 @@
+// type conversion = change the data type of a value to another
+//(strings, numbers, Boolean).
+/*
+let age = window.prompt('How old are you');
+age = Number(age);
+age += 1;
+console.log('Happy Birthday!  You are ', age, 'years old');
+*/
+
+let x;
+let y;
+let z;
+
+x
+x = Number('3.24');
+console.log(x, typeof(x))
