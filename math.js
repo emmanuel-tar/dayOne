2a4ac7a14e2a7c4790f94383766f105b8e1bf8d6
Emmanuel <emmanueltar14@gmail.com>
2023-03-22 15:26:33 +0100

    calculate

--- math.js
+++ math.js
@@ -27,15 +27,16 @@ b = Number(b)
 
 c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
 c = console.log('Side C: ', c)*/
-document.getElementById('submitbtn').onclick = function() {
-    a = document.getElementById('atxt').value
+document.getElementById("submitbtn").onclick = function() {
+
+    a = document.getElementById("atxt").value
     a = Number(a)
 
-    document.getElementById('btxt').value
+    document.getElementById("btxt").value
     b = Number(b)
 
     c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
 
-    document.getElementById('clbl').innerHTML = 'Side C: ' + c
+    document.getElementById("clbl").innerHTML = "Side C: " + c
 
 }
