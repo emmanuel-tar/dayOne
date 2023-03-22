b42b21d3d7bfcf2d04b917922b740a93ac176405
Emmanuel <emmanueltar14@gmail.com>
2023-03-22 07:32:17 +0100

    calculation of the area of triangle

---
+++ math.js
@@ -0,0 +1,14 @@
+let x = -3.142
+let y = 5
+let z = 9
+    //x = Math.floor(x)
+    //x = Math.ceil(x) rounding to the closet
+    //x = Math.pow(x, 3) power exponential
+    //x = Math.sqrt(x) square root
+x = Math.abs(x)
+maximum = Math.max(x, y, z) // maximum number among x,y,z
+minimum = Math.min(x, y, z) // minimum number among x,y,z
+x = Math.PI //PI function in the Math Library
+console.log(x)
+console.log('Maximum :' + maximum)
+console.log('Minimum :' + minimum)
