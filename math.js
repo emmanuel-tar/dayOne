9d5e3fffe1547f28619f21ed1d6161fe4c6f25e8
Emmanuel <emmanueltar14@gmail.com>
2023-03-22 14:39:13 +0100

    Calculate the Area of a trinagle and accepting the value via user input

--- math.js
+++ math.js
@@ -1,3 +1,4 @@
+/*
 let x = -3.142
 let y = 5
 let z = 9
@@ -12,3 +13,29 @@ x = Math.PI //PI function in the Math Library
 console.log(x)
 console.log('Maximum :' + maximum)
 console.log('Minimum :' + minimum)
+*/
+let a
+let b
+let c
+
+/*
+    a = window.prompt('Enter Value for Side A')
+a = Number(a)
+
+b = window.prompt('Enter Value forSide b')
+b = Number(b)
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+c = console.log('Side C: ', c)*/
+document.getElementById('submitbtn').onclick = function() {
+    a = document.getElementById('atxt').value
+    a = Number(a)
+
+    document.getElementById('btxt').value
+    b = Number(b)
+
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+
+    document.getElementById('clbl').innerHTML = 'Side C: ' + c
+
+}
